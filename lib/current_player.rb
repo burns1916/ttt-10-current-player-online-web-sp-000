78a97fb3817eb9e08670f3a3c9b1719247d0ae9a
Ryan K Burns <ryankburns@gmail.com>
2020-09-11 03:38:02 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,9 +12,9 @@ end
 
 def current_player(board)
   turn_count(board)
-  if turn_count(board) % 2
+  if turn_count(board).odd?
     return "X"
-  elsif turn_count(board) !% 2
+  elsif turn_count(board).even?
     return "O"
 end
 end
